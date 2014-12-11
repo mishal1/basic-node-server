4ca056e606c6bb21f03ab466ecd09df5f4f341d0
Mishal Islam <mishal.islam01@gmail.com>
2014-12-11 09:50:19 +0000

    github API data accessed by server

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var bodyParser = require('body-parser');
+var api = require('./src/api.js');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(request, response){
-  response.render('index.ejs', {name: null});
+  console.log(api)
+  response.render('index.ejs', {name: null, api: api});
 });
 
 app.post('/', function(request, response){
@@ -14,7 +16,7 @@ app.post('/', function(request, response){
 });
 
 server.listen(3000, function(){
-  console.log("Server listening on port 3000")
+  console.log("Server listening on port 3000");
 });
 
 module.exports = server;
---
+++ src/api.js
@@ -0,0 +1,68 @@
+var githubApi= { 
+  "total_count": 1,
+  "incomplete_results": false,
+  "items": [
+    {
+      "login": "tansaku",
+      "id": 30216,
+      "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+      "gravatar_id": "",
+      "url": "https://api.github.com/users/tansaku",
+      "html_url": "https://github.com/tansaku",
+      "followers_url": "https://api.github.com/users/tansaku/followers",
+      "following_url": "https://api.github.com/users/tansaku/following{/other_user}",
+      "gists_url": "https://api.github.com/users/tansaku/gists{/gist_id}",
+      "starred_url": "https://api.github.com/users/tansaku/starred{/owner}{/repo}",
+      "subscriptions_url": "https://api.github.com/users/tansaku/subscriptions",
+      "organizations_url": "https://api.github.com/users/tansaku/orgs",
+      "repos_url": "https://api.github.com/users/tansaku/repos",
+      "events_url": "https://api.github.com/users/tansaku/events{/privacy}",
+      "received_events_url": "https://api.github.com/users/tansaku/received_events",
+      "type": "User",
+      "site_admin": false,
+      "score": 111.84937
+    },
+    {
+      "login": "mishal1",
+      "id": 8863744,
+      "avatar_url": "https://avatars.githubusercontent.com/u/8863744?v=3",
+      "gravatar_id": "",
+      "url": "https://api.github.com/users/mishal1",
+      "html_url": "https://github.com/mishal1",
+      "followers_url": "https://api.github.com/users/mishal1/followers",
+      "following_url": "https://api.github.com/users/mishal1/following{/other_user}",
+      "gists_url": "https://api.github.com/users/mishal1/gists{/gist_id}",
+      "starred_url": "https://api.github.com/users/mishal1/starred{/owner}{/repo}",
+      "subscriptions_url": "https://api.github.com/users/mishal1/subscriptions",
+      "organizations_url": "https://api.github.com/users/mishal1/orgs",
+      "repos_url": "https://api.github.com/users/mishal1/repos",
+      "events_url": "https://api.github.com/users/mishal1/events{/privacy}",
+      "received_events_url": "https://api.github.com/users/mishal1/received_events",
+      "type": "User",
+      "site_admin": false,
+      "score": 59.62904
+    },
+   {
+      "login": "nickbdyer",
+      "id": 8281177,
+      "avatar_url": "https://avatars.githubusercontent.com/u/8281177?v=3",
+      "gravatar_id": "",
+      "url": "https://api.github.com/users/nickbdyer",
+      "html_url": "https://github.com/nickbdyer",
+      "followers_url": "https://api.github.com/users/nickbdyer/followers",
+      "following_url": "https://api.github.com/users/nickbdyer/following{/other_user}",
+      "gists_url": "https://api.github.com/users/nickbdyer/gists{/gist_id}",
+      "starred_url": "https://api.github.com/users/nickbdyer/starred{/owner}{/repo}",
+      "subscriptions_url": "https://api.github.com/users/nickbdyer/subscriptions",
+      "organizations_url": "https://api.github.com/users/nickbdyer/orgs",
+      "repos_url": "https://api.github.com/users/nickbdyer/repos",
+      "events_url": "https://api.github.com/users/nickbdyer/events{/privacy}",
+      "received_events_url": "https://api.github.com/users/nickbdyer/received_events",
+      "type": "User",
+      "site_admin": false,
+      "score": 61.870655
+    }
+  ]
+}
+
+module.exports = githubApi
